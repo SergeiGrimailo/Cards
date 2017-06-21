efdbd8894480f240dc48d56b47f2b02f2cb17032
SergeiGrimailo <sgrimailo@gmail.com>
2017-06-21 10:25:52 +0300

    [ADD] Added cards-like background to cards.

--- Sources/Cards/app/src/main/java/com/gmail/sgrimailo/cards/CardsPlayerFragment.java
+++ Sources/Cards/app/src/main/java/com/gmail/sgrimailo/cards/CardsPlayerFragment.java
@@ -1,6 +1,7 @@
 package com.gmail.sgrimailo.cards;
 
 
+import android.animation.Animator;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -9,6 +10,8 @@ import android.support.v7.app.AppCompatDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -24,7 +27,10 @@ public class CardsPlayerFragment extends AppCompatDialogFragment {
     public static final String ARG_CARD_ID = String.format("%s.%s",
             CardsPlayerFragment.class.getName(), "ARG_CARD_ID");
 
+    private static final int ANIMATION_DURATION = 1000;
+
     private boolean mShowingBack = true;
+    private FrameLayout mCardSidesHolder;
 
     public CardsPlayerFragment() {
     }
@@ -54,32 +60,64 @@ public class CardsPlayerFragment extends AppCompatDialogFragment {
         TextView tvBackSide = (TextView) getView().findViewById(R.id.tvBackSideContent);
         tvBackSide.setText(cardCursor.getString(cardCursor.getColumnIndex(Cards.COLUMN_BACK_SIDE)));
 
-        flipCard();
+        flipCardSides();
 
-        FrameLayout panelCardsHolder = (FrameLayout) getView().findViewById(R.id.lCardsHolder);
-        panelCardsHolder.setOnClickListener(new View.OnClickListener() {
+        mCardSidesHolder = (FrameLayout) getView().findViewById(R.id.lCardSidesHolder);
+        mCardSidesHolder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                flipCard();
+                flipCardSidesAnimated();
             }
         });
-
     }
 
-    private void flipCard() {
+    private void flipCardSides() {
 
-        View front = getView().findViewById(R.id.lFrontSide);
-        View back = getView().findViewById(R.id.lBackSide);
+        final View front = getView().findViewById(R.id.lFrontSide);
+        final View back = getView().findViewById(R.id.lBackSide);
 
         if (mShowingBack) {
-            front.setVisibility(View.VISIBLE);
             back.setVisibility(View.INVISIBLE);
+            front.setVisibility(View.VISIBLE);
         } else {
             front.setVisibility(View.INVISIBLE);
             back.setVisibility(View.VISIBLE);
         }
 
         mShowingBack = !mShowingBack;
+    }
+
+    private void flipCardSidesAnimated() {
+        mCardSidesHolder.animate().rotationXBy(90f).setDuration(ANIMATION_DURATION)
+                .setInterpolator(new AccelerateInterpolator()).setListener(
+                new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        flipCardSides();
+                        mCardSidesHolder.setRotationX(-90f);
+                        mCardSidesHolder.animate().setInterpolator(new DecelerateInterpolator()).setListener(null);
+                        mCardSidesHolder.animate().rotationXBy(90f).setDuration(ANIMATION_DURATION).start();
+                    }
+                }
+        ).start();
+    }
+
+    class AnimatorListenerAdapter implements Animator.AnimatorListener {
+        @Override
+        public void onAnimationStart(Animator animation) {
+        }
+
+        @Override
+        public void onAnimationEnd(Animator animation) {
+        }
+
+        @Override
+        public void onAnimationCancel(Animator animation) {
+        }
+
+        @Override
+        public void onAnimationRepeat(Animator animation) {
+        }
     }
 
 }
